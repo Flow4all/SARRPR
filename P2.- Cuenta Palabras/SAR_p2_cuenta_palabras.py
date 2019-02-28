b4f648270caf6448292ba8aa6561e497f457a3e4
javispp <34429055+javispp@users.noreply.github.com>
2019-02-28 22:56:39 +0100

    Add files via upload

--- P2.- Cuenta Palabras/SAR_p2_cuenta_palabras.py
+++ P2.- Cuenta Palabras/SAR_p2_cuenta_palabras.py
@@ -4,6 +4,9 @@ from operator import itemgetter
 import re
 import sys
 
+#Hecho por José Cantó Moscardó y Javier López Sanz
+#Ej ampliación realizado
+
 clean_re = re.compile('\W+')
 def clean_text(text):
     return clean_re.sub(' ', text)
@@ -12,20 +15,22 @@ def sort_dic(d):
     for key, value in sorted(d.items(), key=itemgetter(1), reverse=True):
         yield key, value
 
-def text_statistics(filename, to_lower, remove_stopwords):
+def text_statistics(filename, to_lower, remove_stopwords, extra):
     fichero=open(filename)
     texto=fichero.read()
     if to_lower:
         texto = texto.lower()
     texto_limpio=clean_text(texto)
     palabras=texto_limpio.split(" ")
+    lineas=texto.split("\n")
+    
     if remove_stopwords:
         stopwords = (open("stopwords_en.txt")).read().split("\n")
         aux=[]
         for palabra in palabras:
             if not palabra in stopwords:
                 aux.append(palabra)
-    lineas=texto.split("\n")
+    
     lines = len(lineas)
     words = len(palabras)
            
@@ -36,7 +41,8 @@ def text_statistics(filename, to_lower, remove_stopwords):
         vocabulario=aux
     else:
         vocabulario=palabras
-    for palabra in vocabulario:
+        
+    for palabra in vocabulario: 
         for i in range(len(palabra)):
     	      letra = palabra[i]
     	      s[letra] = s.get(letra,0) + 1
@@ -65,6 +71,44 @@ def text_statistics(filename, to_lower, remove_stopwords):
     freq = sort_dic(s)
     for i in freq:
         print("    "+str(i))
+    if extra:
+        lineas=texto_limpio.split("\n")
+        for i in range(len(lineas)): 
+            if i!=len(lineas)-1:
+                lineas[i]="$ "+lineas[i]+"$"
+            else:
+                lineas[i]="$ "+lineas[i]+" $"
+            clean_list=lineas[i].split(" ") 
+            if remove_stopwords:
+                aux=[]
+                for j in range(len(clean_list)):
+                    if not clean_list[j] in stopwords:
+                        aux.append(clean_list[j])
+                lineas[i]=" ".join(aux)
+        d2={}
+        s2={}
+        for i in lineas:
+            palabras=i.split(" ")
+            for j in range(len(palabras)-1):
+                d2[palabras[j]+" "+palabras[j+1]]=d2.get(palabras[j]+" "+palabras[j+1],0) + 1
+                for k in range(len(palabras[j])-1):
+                    if j!=0:
+                        s2[palabras[j][k]+palabras[j][k+1]]=s2.get(palabras[j][k]+palabras[j][k+1],0) + 1
+                
+        print("Word pairs (alphabetical order):")
+        for i in sorted(d2.keys()):
+            print("    ("+str(i)+","+str(d2[i])+")")
+        print("Word pairs(frequency order):")
+        freq = sort_dic(d2)
+        for i in freq:
+            print("    "+str(i))
+        print("Symbol pair(alphabetical order):")
+        for i in sorted(s2.keys()):
+            print("    ("+str(i)+","+str(s2[i])+")")
+        print("Symbol pairs(frequency order):")
+        freq = sort_dic(s2)
+        for i in freq:
+            print("    "+str(i))
 
 def syntax():
     print ("\n%s filename.txt [to_lower?[remove_stopwords?]\n" % sys.argv[0])
@@ -74,8 +118,11 @@ if __name__ == "__main__":
     name = sys.argv[1]
     lower = False
     stop = False
+    extra = False
     if len(sys.argv) > 2:
         lower = (sys.argv[2] in ('1', 'True', 'yes'))
         if len(sys.argv) > 3:
             stop = (sys.argv[3] in ('1', 'True', 'yes'))
-    text_statistics(name, lower, stop)
+            if len(sys.argv) > 4 and sys.argv[4]=='extra':
+                extra=True
+    text_statistics(name, lower, stop, extra)
